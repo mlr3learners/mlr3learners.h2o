f9f0d61010ada87892a9f9adcfdf59e52f470496
be-marc <marcbecker@posteo.de>
2020-03-25 00:18:59 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -5,12 +5,27 @@
 #' @importFrom mlr3 mlr_learners LearnerClassif LearnerRegr
 "_PACKAGE"
 
-.onLoad = function(libname, pkgname) {
-  # nocov start
+# nocov start
+register_mlr3 = function(libname, pkgname) {
   # get mlr_learners dictionary from the mlr3 namespace
   x = utils::getFromNamespace("mlr_learners", ns = "mlr3")
 
   # add the learner to the dictionary
   x$add("classif.h2odeeplearning", LearnerClassifH2ODeeplearning)
   x$add("regr.h2odeeplearning", LearnerRegrH2ODeeplearning)
-} # nocov end
+}
+
+.onLoad = function(libname, pkgname) {
+  register_mlr3()
+  setHook(packageEvent("mlr3", "onLoad"), function(...) register_mlr3(),
+          action = "append")
+}
+
+.onUnload = function(libpath) {
+  event = packageEvent("mlr3", "onLoad")
+  hooks = getHook(event)
+  pkgname = vapply(hooks, function(x) environment(x)$pkgname, NA_character_)
+  setHook(event, hooks[pkgname != "mlr3learners.h2o"],
+          action = "replace")
+}
+# nocov end
