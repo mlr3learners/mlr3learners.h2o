06c0711ac83cb943281ab154433223d0e1709d1b
be-marc <marcbecker@posteo.de>
2020-03-25 21:27:30 +0100

    Fix tests

--- tests/testthat/test_classif_h2oglm.R
+++ tests/testthat/test_classif_h2oglm.R
@@ -3,8 +3,6 @@ context("classif.h2oglm")
 test_that("autotest", {
   learner = LearnerClassifH2OGLM$new()
   expect_learner(learner)
-  # seed must be implemented for feat_all
-  # data set too small for weights_binary
-  result = run_autotest(learner, exclude = "(weights|weights_binary|weights_multiclass|feat_all)")
+  result = run_autotest(learner, exclude = "feat_all")
   expect_true(result, info = result$error)
 })
--- tests/testthat/test_regr_h2ogbm.R
+++ tests/testthat/test_regr_h2ogbm.R
@@ -3,6 +3,8 @@ context("regr.h2ogbm")
 test_that("autotest", {
   learner = LearnerRegrH2OGBM$new()
   expect_learner(learner)
-  result = run_autotest(learner)
+  # seed must be implemented for feat_all
+  # data set too small for weights
+  result = run_autotest(learner, exclude = "(weights|weights_binary|weights_multiclass|feat_all)")
   expect_true(result, info = result$error)
 })
